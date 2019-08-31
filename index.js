38b42b94b86b860ed474a17f3f21f162d59785a2
patorma <pocontreras@ing.ucsc.cl>
2019-08-31 02:20:40 -0400

    se creo carpeta routes

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 
 //crear una app de express
 const app = express()
 
-//Ejemplo arreglo estatico como de bd
-const productos = [{
-        producto: 'Libro',
-        precio: 20
-    },
-    {
-        producto: 'Computadora',
-        precio: 10000
-    }
-]
+app.use('/', routes())
 
-//ruta para el home
-app.use('/', (req, res) => {
-    res.json(productos)
-})
 
 app.listen(3000)
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+module.exports = function() { // equivalente a export default
+    //ruta para el home
+    router.get('/', (req, res) => {
+        res.send('Index')
+    })
+
+    router.get('/nosotros', (req, res) => {
+        res.send('Nosotros')
+    })
+
+    return router
+}
