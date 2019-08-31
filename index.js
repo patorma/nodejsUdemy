bdfd0e40ae04226d20759aa8c69d65cb2b03b347
patorma <pocontreras@ing.ucsc.cl>
2019-08-30 21:23:41 -0400

    se vio ejemplo de mostrar datos de una bd

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const express = require('express')
 //crear una app de express
 const app = express()
 
+//Ejemplo arreglo estatico como de bd
+const productos = [{
+        producto: 'Libro',
+        precio: 20
+    },
+    {
+        producto: 'Computadora',
+        precio: 10000
+    }
+]
+
 //ruta para el home
 app.use('/', (req, res) => {
-    res.send('Hola')
+    res.json(productos)
 })
 
 app.listen(3000)
